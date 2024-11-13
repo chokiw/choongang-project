b94f58496bde6134ac7667bfe5f619e159d20c6a
hyunho2668 <black2660@gmail.com>
2024-11-13 18:45:20 +0900

    내가 추천한 글 목록 최종 수정 liked_board, liked_list 추가 그에 따른 컨트롤러,dao,서비스 변경

--- testproject/src/main/java/com/example/demo/service/ProjectService.java
+++ testproject/src/main/java/com/example/demo/service/ProjectService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -73,8 +75,18 @@ public class ProjectService {
 		return dao.findpass(runner);
 	}
 
-	public List<SnsBoard> getLikedPostsByUserId(String user_id) {
-		return dao.getLikedPostsByUserId(user_id);
+	public int getTotalLiked(String user_id) {
+
+		return dao.getTotalLiked(user_id);
 	}
 
+	public List<SnsBoard> getLikedPosts(String user_id, int startRow, int endRow) {
+		// 매개변수들을 Map에 담아서 전달
+		Map<String, Object> params = new HashMap<>();
+		params.put("user_id", user_id);
+		params.put("startRow", startRow);
+		params.put("endRow", endRow);
+
+		return dao.getLikedPosts(params);
+	}
 }
